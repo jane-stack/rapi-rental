5fe3dc06837702b32e794704d92222552c9548f9
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-28 13:04:01 -0600

    password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,39 @@
 class User < ApplicationRecord
     has_secure_password
 
-    has_many :cars
+    has_many :user_cars, foreign_key: "owner_id", class_name: "Car"
+    has_many :cars, dependent: :destroy
+    has_many :car_reviews, through: :cars, source: :car
 
+    before_save { email.downcase! }
+    before_save { last_name.downcase! }
+    before_save { first_name.downcase! }
     validates :last_name, :first_name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
-    validates :password, presence: true, confirmation: true
+    validates :password, presence: true, confirmation: true, length: { minimum: 8 }
+
+    validate :password_requirements
+
+    private
+
+    def password_requirements
+        return if password.blank?
+
+        unless password.match?(/[a-z]/)
+            errors.add(:password, "must contain one lowercase letter")
+        end
+
+        unless password.match?(/[A-Z]/)
+            errors.add(:password, "must contain one uppercase letter")
+        end
+
+        unless password.match?(/\d/)
+            errors.add(:password, "must contain one digit")
+        end
+
+        unless password.match?(/[[:^alnum:]]/)
+            errors.add(:password, "must contain one special character")
+        end
+    end
 end
