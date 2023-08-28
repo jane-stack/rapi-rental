d28d32b98a8b13dae17c8d5be7a938a724d01782
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-28 13:08:00 -0600

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+
+    def logged_in?
+        session[:user_id]
+    end
+
+    def current_user
+        user = User.find_by_id(session[:user_id])
+    end
+
+    def authorize
+        render json: { errors: ["Please login"] }, status: :unauthorized unless logged_in?
+    end
+
+    def authorize_user_resource(user_id)
+        render json: { errors: ["You must be logged in"] }, status: :unauthorized unless user_id === current_user.id
+    end
 end
