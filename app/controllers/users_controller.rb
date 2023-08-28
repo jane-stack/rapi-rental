1030d04c09e827727c0c6279a3a9d9125295b970
Tuyen Ha <98144714+jane-stack@users.noreply.github.com>
2023-08-28 13:13:09 -0600

    user controller updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,22 @@
 class UsersController < ApplicationController
+
+    def show
+        render json: current_user
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            render json: @user, status: 201
+        else
+            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:last_name, :first_name, :email, :password, :password_confirmation)
+    end
 end
