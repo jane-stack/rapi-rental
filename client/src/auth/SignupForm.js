5e32d842b623a04fa9dfff0c5eab5f0f5f7db9fe
janenguyen <tuyen_ha@outlook.com>
2023-08-28 20:33:53 -0600

    still having first name and last name error messages

--- client/src/auth/SignupForm.js
+++ client/src/auth/SignupForm.js
@@ -1,8 +1,78 @@
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../context/UserContext";
+import { ErrorContext } from "../context/ErrorContext";
+import { useNavigate } from "react-router-dom";
+import Errors from "../errors/Errors";
+
 function SignupForm() {
+    const { addUser, loginUser, loggedIn } = useContext(UserContext);
+    const { setErrors } = useContext(ErrorContext);
+    const navigate = useNavigate();
+    const [lastName, setLastName] = useState("");
+    const [firstName, setFirstName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+    useEffect(() => {
+        if (loggedIn) {
+            navigate("/")
+        } else {
+            return (
+                setErrors([])
+            )
+        }
+    }, [loggedIn, navigate, setErrors])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetch('/signup', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({
+                lastName: lastName, 
+                firstName: firstName, 
+                email: email, 
+                password: password, 
+                passwordConfirmation: passwordConfirmation 
+            })
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.errors) {
+                setErrors(data.errors);
+            } else {
+                addUser(data);
+                loginUser(data);
+                navigate('/');
+            }
+        });
+    };
 
     return (
-        <form>
-            SIGNUP HERE
+        <form onSubmit={handleSubmit}>
+            <div>
+                Last Name &nbsp;
+                <input type="text" name="lastName" id="lastName" value={lastName} onChange={(e) => setLastName(e.target.value)}/>
+            </div>
+            <div>
+                First Name &nbsp;
+                <input type="text" name="firstName" id="firstName" value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+            </div>
+            <div>
+                Email &nbsp;
+                <input type="text" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </div>
+            <div>
+                Password &nbsp;
+                <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+            <div>
+                Confirm Password &nbsp;
+                <input type="password" name="passwordConfirmation" id="passwordConfirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)}/>
+            </div>
+            <button type="submit">Sign Up</button>
+            <Errors />
         </form>
     )
 }
